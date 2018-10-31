365a8b21e23867918eaba2adfee8941c41e5bc7c
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2018-10-31 14:58:28 +0900

    Make fragment_detail.xml work in tablet

--- app/src/main/java/com/example/android/bakingapp/ui/activity/DetailActivity.java
+++ app/src/main/java/com/example/android/bakingapp/ui/activity/DetailActivity.java
@@ -11,9 +11,9 @@ import dagger.android.AndroidInjection;
 public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        AndroidInjection.inject(this);
     }
 
     @Override
